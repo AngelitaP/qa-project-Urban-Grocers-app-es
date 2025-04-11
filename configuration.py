d1277975fae84b58e8b93c92808fd143ffcac6d6
AngelitaP <jsebastiansilva@hotmail.es>
2025-04-10 20:16:24 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-9c15cd10-1d37-4c34-a4ac-9bda9d7d8a82.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-94eaba17-1532-4f55-ade9-5270b4ddc62b.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
