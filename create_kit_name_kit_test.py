9d6759bd6b5fa4c1c0c0f73ba4e46358e02248ec
AngelitaP <jsebastiansilva@hotmail.es>
2025-04-10 20:16:26 -0500

    Update create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,13 +1,37 @@
 import sender_stand_request
 import data
 
-
 def get_kit_body(name):
     current_body = data.kit_body.copy()
     current_body["name"] = name
     return current_body
 
+def get_new_user_token():
+    user_body = data.user_body
+    resp_user = sender_stand_request.post_new_user(user_body)
+    return resp_user.json()["authToken"]
+
 def positive_assert(kit_body):
-    kit_body = get_kit_body(name)
-    kit_response = sender_stand_request.post_new_user(kit_body)
-    assert kit_response.status_code == 201
+    response = sender_stand_request.post_new_client_kit(kit_body, get_new_user_token())
+    assert response.status_code == 201
+    assert response. json()["name"] == kit_body["name"]
+
+def negative_assert_400(kit_body):
+    response = sender_stand_request.post_new_client_kit(kit_body, get_new_user_token())
+    assert response.status_code == 400
+    assert response.json()["name"] == kit_body["name"]
+
+def test_numero_permitido_de_caracteres_1():
+    positive_assert("a")
+
+def test_numero_permitido_de_caracteres_511():
+    positive_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
+
+def test_numero_permitido_de_caracteres_0():
+    positive_assert("")
+
+def test_numero_permitido_de_caracteres_512():
+    positive_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
+
+def test_numero_caracteres_especiales_permitidos():
+    positive_assert("№%@,")
