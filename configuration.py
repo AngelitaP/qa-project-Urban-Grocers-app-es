98de87106b870cd7a9c7aa59ffb3d059ed8f0028
Angela Pirachican <angelita.admon@outlook.com>
2025-04-11 20:25:48 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-48ad9744-7ef7-4f70-be19-5ad05328340c.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-1399f5e5-9cac-4dcf-9543-88bf43bbb4e5.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
